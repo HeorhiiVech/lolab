68656b211af2591bbcc12f0f119c75e3ddee792f
heouol <youshipertube@gmail.com>
2025-08-08 16:24:09 +0200

    Integrate Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from "react-ga4"; // Импортируем библиотеку
 import './index.css';
 import App from './App';
+
+// Инициализируем Google Analytics с твоим ID
+ReactGA.initialize("G-XST184R82M");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
